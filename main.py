b01df8145227eda651928c81d4bfadc6f39ed5b4
Ericson Etienne Jr. <liminalgrimoire@gmail.com>
2024-12-28 22:09:32 -0700

    Added player.py, and the base methods for displaying the player on the screen

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 #Allow the use of code from the open-source pygame library.
 import pygame
 from constants import *
+from player import *
+
+
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0 #Represents time since the last frame was drawn.
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player_one = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None)
+        player_one.draw(screen)
         pygame.display.flip()
         game_clock.tick(60)
         dt = game_clock.tick()/1000 #Convert from ms to s.
